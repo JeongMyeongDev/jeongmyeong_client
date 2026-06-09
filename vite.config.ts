2967d5aa82ec03970a82823a890be2b3e02b9126
user <dlgusdnse2008@gmail.com>
2026-06-09 23:44:14 +0900

    fix: update API base URL and refine Vite server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['jeongmyeong.mirim-it-show.site'], 
+    allowedHosts: ['jeongmyeong.mirim-it-show.site'],
+    hmr: {
+      protocol: 'wss',
+      host: 'jeongmyeong.mirim-it-show.site',
+      clientPort: 443,
+    },
   },
 })
